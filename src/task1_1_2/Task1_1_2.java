a7632186a0eeda02d6feffc1e9f789e80b2e1085
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2024-02-20 19:15:38 +0300

    Initial Commit

--- src/task1_1_2/Task1_1_2.java
+++ src/task1_1_2/Task1_1_2.java
@@ -1,12 +1,13 @@
-package task1_1_1;
+package task1_1_2;
 
-public class Task1_1_1 {
+public class Task1_1_2 {
     public static void main(String[] args) {
         System.out.println("""
             Модуль 1. Введение в программирование. Задание №1:\s
                 2. Выведите любимое стихотворение в консоль\s
 
             Решение:\s""");
-        System.out.println("Меня зовут Владислав.");
+        System.out.println("Белеет парус одинокий" +
+                "\nВ тумане моря голубом...");
     }
 }
