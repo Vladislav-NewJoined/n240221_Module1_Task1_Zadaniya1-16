9b186c1c0c77cd62e33900d946fc849fd0885b05
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2024-02-20 17:35:11 +0300

    Initial Commit

--- src/task1_1_1/Task1_1_1.java
+++ src/task1_1_1/Task1_1_1.java
@@ -1,2 +1,12 @@
-package task1_1_1;public class Task1_1_1 {
+package task1_1_1;
+
+public class Task1_1_1 {
+    public static void main(String[] args) {
+        System.out.println("""
+            Модуль 1. Введение в программирование. Задание №1:\s
+                1. Выведите ваше имя в консоль\s
+
+            Решение:\s""");
+
+    }
 }
